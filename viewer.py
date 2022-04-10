446e0a1935358aafd6aae6723396a7363b1906eb
nccvector <fzn1020@gmail.com>
2022-04-10 11:28:54 +0500

    new comm

--- viewer.py
+++ viewer.py
@@ -14,6 +14,10 @@ for f in glob('./archive/*'):
         for i in range(dataset.pixel_array.shape[0]):
             cv2.imshow("Image", dataset.pixel_array[i, :, :, 0])
             cv2.waitKey(66)
+
+        cv2.imwrite(f + '.jpg', dataset.pixel_array[int(i/2), :, :, 0], [cv2.IMWRITE_JPEG_QUALITY, 100])
     else:
         cv2.imshow("Image", dataset.pixel_array[:, :, :])
         cv2.waitKey(0)
+        cv2.imwrite(f + '.jpg', dataset.pixel_array[:, :, :], [cv2.IMWRITE_JPEG_QUALITY, 100])
+    
